17c4f2ca7884e1ee59263c4ee16fad7cfa665976
cdapazav <cristofer16apaza@gmail.com>
2021-10-04 02:27:25 -0400

    Agregar archivos de proyecto.

---
+++ EstructuraPilas/Pila.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstructuraPilas
+{
+    class Pila
+    {
+        static int maxpila = 5;
+        public int tope;
+        public int[] lista;
+
+        public Pila() 
+        {
+            tope = -1;
+            lista = new int[maxpila];
+        }
+
+        private bool vacia()
+        {
+            if (tope == -1)
+                return true;
+            else
+                return false;
+         }
+        
+        private bool llena() 
+        {
+            if (tope == maxpila - 1)
+                return true;
+            else
+                return false;
+        }
+
+        public void insertar(int elemento)
+        {
+            if (llena())
+                MessageBox.Show("La pila esta llena, no se puede insertar mas elementos");
+            else
+            {
+                tope++;
+                lista[tope] = elemento;
+            }
+        }
+
+        public void eliminar()
+        {
+            if (vacia())
+                MessageBox.Show("La pila esta vacia, no se puede eliminar elementos");
+            else
+            {
+                MessageBox.Show("Elemento eliminado es: " + lista[tope]);
+                tope--;
+            }
+        }
+    }
+}
